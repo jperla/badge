3d071e902d4629b5640d7a0f05cbdaf2963fe2af
Brandon Podmayersky <branclem@gmail.com>
2012-05-05 16:57:43 -0400

    Introductory Bluetooth functionality.

--- BadgeProject/src/com/jperla/badge/BadgeActivity.java
+++ BadgeProject/src/com/jperla/badge/BadgeActivity.java
@@ -10,6 +10,17 @@ import android.hardware.SensorEvent;
 import android.os.Bundle;
 import android.widget.ViewSwitcher;
 
+import android.util.Log;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.bluetooth.BluetoothServerSocket;
+import android.content.Intent;
+import java.util.UUID;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class BadgeActivity extends Activity implements SensorEventListener
 {
 
@@ -19,6 +30,12 @@ public class BadgeActivity extends Activity implements SensorEventListener
     Sensor mag_sensor;
     float[] acceleration_vals = null;
     float[] magnetic_vals = null;
+
+    BluetoothAdapter bt_adapter;
+
+    static final int BT_ENABLE_ACTIVITY = 1;
+    static final UUID BT_UUID = UUID.fromString("ff15e609-34b1-4b49-9d40-a650566c8960");
+    static final String joe_mac = "9C:02:98:70:23:67";
 
     /** Called when the activity is first created. */
     @Override
@@ -42,6 +59,19 @@ public class BadgeActivity extends Activity implements SensorEventListener
         mag_sensor = sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         sm.registerListener(this, mag_sensor, SensorManager.SENSOR_DELAY_NORMAL);
 
+
+
+        // Fetch the Bluetooth adapter and make sure it is enabled.
+        bt_adapter = BluetoothAdapter.getDefaultAdapter();
+        if(bt_adapter == null) {
+            Log.d("=badge=", "ERROR: No Bluetooth adapter found");
+        }
+        else {
+            Log.d("=badge=", "Successfully found Bluetooth adapter");
+        }
+
+        Log.d("=badge=", BT_UUID.toString());
+
     }
 
     @Override
@@ -55,6 +85,16 @@ public class BadgeActivity extends Activity implements SensorEventListener
         super.onResume();
         sm.registerListener(this, acc_sensor, SensorManager.SENSOR_DELAY_NORMAL);
         sm.registerListener(this, mag_sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        if (bt_adapter.isEnabled()) {
+            Log.d("=badge=", "Bluetooth already enabled");
+            tryConnection();
+        }
+        else {
+            Log.d("=badge=", "Requesting to enable Bluetooth");
+            Intent enable_bt = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enable_bt, BT_ENABLE_ACTIVITY);
+        }
     }
 
     public void onAccuracyChanged(Sensor sensor, int accuracy)
@@ -105,6 +145,79 @@ public class BadgeActivity extends Activity implements SensorEventListener
                 showBadgeView();
             }
         }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        switch (requestCode)
+        {
+            case BT_ENABLE_ACTIVITY:
+                if (resultCode == RESULT_OK) {
+                    Log.d("=badge=", "Bluetooth successfully enabled");
+
+                    // Connect to my laptop to test.
+                    tryConnection();
+                }
+                else {
+                    Log.d("=badge=", "ERROR: Could not enable Bluetooth.");
+                }
+                break;
+        }
+    }
+
+    void tryConnection()
+    {
+
+        String mac = bt_adapter.getAddress();
+
+        if(mac.equals(joe_mac)) {
+            // Act as server.
+            try {
+                BluetoothServerSocket ss = bt_adapter.listenUsingRfcommWithServiceRecord("badge", BT_UUID);
+                Log.d("=badge=", "Got server socket");
+                BluetoothSocket s = ss.accept();
+                Log.d("=badge=", "Accepted connection");
+                
+                ss.close();
+
+                byte[] buffer = new byte[1024];
+                int bytes;
+                InputStream in = s.getInputStream();
+                bytes = in.read(buffer);
+                for (int i = 0; i < bytes; i++) {
+                    Log.d("=badge=", "bytes[" + i + "]" + " = " + buffer[i]);
+                }
+
+                s.close();
+                Log.d("=badge=", "Closed socket");
+            }
+            catch (IOException e) {
+                Log.d("=badge=", "ERROR: IOException:" + e.toString());
+            }
+        }
+        else {
+            // Act as client.
+            BluetoothDevice dev = bt_adapter.getRemoteDevice(joe_mac);
+            Log.d("=badge=", "Attempting to connect");
+            try {
+                BluetoothSocket s = dev.createRfcommSocketToServiceRecord(BT_UUID);
+                Log.d("=badge=", "Created socket");
+                s.connect();
+                Log.d("=badge=", "Returned from connect");
+
+                byte[] bob = {1, 2, 3, 4};
+                OutputStream out = s.getOutputStream();
+                out.write(bob);
+                Log.d("=badge=", "Sent message");
+
+                s.close();
+                Log.d("=badge=", "Closed socket");
+            }
+            catch (IOException e) {
+                Log.d("=badge=", "ERROR: IOException:" + e.toString());
+            }
+        }
+
     }
 
     void showBadgeView()
