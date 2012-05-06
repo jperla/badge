467ce36e0e53478e2f96994da24e164629d3d12a
Zhao <x.zhao.y@gmail.com>
2012-05-06 15:28:12 -0400

    Added FrameMarkers sample to repo

---
+++ FrameMarkers/gen/R.java.d
@@ -0,0 +1,13 @@
+/Users/zhao/Courses/598B/project/badge/FrameMarkers/gen/com/qualcomm/QCARSamples/FrameMarkers/R.java \
+ : /Users/zhao/Courses/598B/project/badge/FrameMarkers/res/drawable/icon_vuforia.png \
+/Users/zhao/Courses/598B/project/badge/FrameMarkers/res/drawable/splash_screen_frame_markers.png \
+/Users/zhao/Courses/598B/project/badge/FrameMarkers/res/drawable-hdpi/icon_vuforia.png \
+/Users/zhao/Courses/598B/project/badge/FrameMarkers/res/drawable-ldpi/icon_vuforia.png \
+/Users/zhao/Courses/598B/project/badge/FrameMarkers/res/drawable-mdpi/icon_vuforia.png \
+/Users/zhao/Courses/598B/project/badge/FrameMarkers/res/values/strings.xml \
+/Users/zhao/Courses/598B/project/badge/FrameMarkers/bin/res/drawable/icon_vuforia.png \
+/Users/zhao/Courses/598B/project/badge/FrameMarkers/bin/res/drawable/splash_screen_frame_markers.png \
+/Users/zhao/Courses/598B/project/badge/FrameMarkers/bin/res/drawable-hdpi/icon_vuforia.png \
+/Users/zhao/Courses/598B/project/badge/FrameMarkers/bin/res/drawable-ldpi/icon_vuforia.png \
+/Users/zhao/Courses/598B/project/badge/FrameMarkers/bin/res/drawable-mdpi/icon_vuforia.png \
+/Users/zhao/Courses/598B/project/badge/FrameMarkers/AndroidManifest.xml \
