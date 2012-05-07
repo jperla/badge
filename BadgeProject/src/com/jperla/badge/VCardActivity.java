0d9db784c36d93f85263016092bb21598cea9a8b
Zhao <x.zhao.y@gmail.com>
2012-05-07 04:26:10 -0400

    Fixed duplicate entry bug

--- BadgeProject/src/com/jperla/badge/VCardActivity.java
+++ BadgeProject/src/com/jperla/badge/VCardActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.jperla.badge.VCard;
@@ -12,6 +14,10 @@ public class VCardActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         Intent intent = getIntent();
         String card = intent.getStringExtra("card");
